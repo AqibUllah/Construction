2c91ad46dc352c3012ea5debdff9f9913fabbd73
AqibUllah <aqibullah3312@gmail.com>
2022-10-25 21:42:14 +0500

    services routes added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ServiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,5 @@ Route::get('/contact', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('service',ServiceController::class);
