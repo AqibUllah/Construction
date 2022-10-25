cd774b8673d8d5fd761917f0ffd457049e97790f
AqibUllah <aqibullah3312@gmail.com>
2022-10-25 21:36:05 +0500

    contact page created

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,27 +1,12 @@
 @extends('layouts.app')
 @section('title','Home')
 
+@section('header')
+<h1>hello</h1>
+@endsection
 @section('content')
-<div id="banner-area" class="banner-area" style="background-image:url(/assets/images/banner/banner1.jpg)">
-  <div class="banner-text">
-    <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-              <div class="banner-heading">
-                <h1 class="banner-title">Contact</h1>
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb justify-content-center">
-                      <li class="breadcrumb-item"><a href="/">Home</a></li>
-                      <!-- <li class="breadcrumb-item"><a href="#">Company</a></li> -->
-                      <li class="breadcrumb-item active" aria-current="page">Contact Us</li>
-                    </ol>
-                </nav>
-              </div>
-          </div><!-- Col end -->
-        </div><!-- Row end -->
-    </div><!-- Container end -->
-  </div><!-- Banner text end -->
-</div><!-- Banner area end --> 
+
+<x-banner title="Contact" />
 
 <section id="main-container" class="main-container">
   <div class="container">
