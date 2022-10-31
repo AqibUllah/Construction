b0637be4b4479d3bd05876c63142496ffaa6b5ef
AqibUllah <aqibullah3312@gmail.com>
2022-10-31 21:23:32 +0500

    vendor dashboard layout ready

--- resources/views/vendor/VendorDashboard.blade.php
+++ resources/views/vendor/VendorDashboard.blade.php
@@ -10,9 +10,21 @@
 </style>
 @endsection
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
+    <div class="row mb-1">
+        <div class="col-md-6">
+            <x-dashboard-card title="My Services" value="3" icon="icon-image/service-icon2.png" />
+        </div><!-- col end -->
+        <div class="col-md-6">
+            <x-dashboard-card title="Active Services" value="3" icon="icon-image/back-in-time.png" />
+        </div><!-- col end -->
+    </div><!-- 1st row end-->
 
+    <div class="row mb-1">
+        <div class="col-md-6">
+            <x-dashboard-card title="Completed" value="2" icon="icon-image/completed1.png" />
+        </div><!-- col end -->
+        <div class="col-md-6">
+            <x-dashboard-card title="Cancelled" value="1" icon="icon-image/cancelled.png" />
         </div><!-- col end -->
     </div><!-- 1st row end-->
 
